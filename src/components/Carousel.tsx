081e297ddccbeb213c471054d2a66b5b16a9131c
DisonUyoga <disonobudho233@gmail.com>
2024-08-01 20:00:00 +0300

    route interception state management

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -34,8 +34,8 @@ const HeroSlider = ({ products }: CarouselProps) => {
     <Box width="100%" height="500px" overflow="hidden">
       <Slider {...settings}>
         {products.map((src, index) => (
-          <Link href={`/product/${src.id}`}>
-            <Box key={index} height="400px" position="relative">
+          <Link key={index} href={`/product/${src.id}`}>
+            <Box height="400px" position="relative">
               <ProductImage
                 fallback={pizzas[0].img as string}
                 path={src.image as string}
